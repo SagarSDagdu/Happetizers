af985b944587a332690f3790e5506bfe35e8d4c7
Sagar Dagdu <shags032@gmail.com>
2021-06-05 17:57:30 +0530

    Add initial code

---
+++ Happetizers/HappetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappetizersApp.swift
+//  Happetizers
+//
+//  Created by Sagar Dagdu on 05/06/21.
+//
+
+import SwiftUI
+
+@main
+struct HappetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
